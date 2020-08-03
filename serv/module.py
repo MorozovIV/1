a043122e68c124f11538e56a07e5e2431c8ce263
morozov <Shadow-zy@yandex.ru>
2020-08-03 10:18:33 +0300

    1

--- serv/module.py
+++ serv/module.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     print("Hello")
-# try:
-def hi():                   #import module
-    print("Привет")         #module.hi
-# except:
-#     pass
+try:
+    def hi():                   #import module
+        print("Привет")         #module.hi
+except:
+     pass

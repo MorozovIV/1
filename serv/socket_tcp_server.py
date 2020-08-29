085d626f56f915e676617559656f0e30aa5367d0
Shadow <shadow-zy@yandex.ru>
2020-08-29 21:14:23 +0300

    serv1

--- serv/socket_tcp_server.py
+++ serv/socket_tcp_server.py
@@ -1,12 +1,17 @@
 import socketserver
 
 class EchoTCPHandler(socketserver.BaseRequestHandler):
-
+qq=data
     def handle(self):
         data = self.request.recv(1024).strip()
         print('Adress: {}'.format(self.client_address[0]))
         print('Data: {}'.format(data.decode()))
         self.request.sendall(data)
+
+if data == "kill bill":
+    print('ok')
+pass
+
 
 if __name__=='__main__':
     with socketserver.TCPServer(('127.0.0.1', 8888), EchoTCPHandler) as server:

7729f32ae259b523b51f9dcc3fe6d9f48a10f54e
Shadow <Shadow-Zy@yandex.ru>
2021-03-12 18:17:12 +0300

    1

---
+++ 001/lesson1.py
@@ -0,0 +1,19 @@
+# i = 0
+# while i < 10:
+#     i += 1
+#     if i == 5:
+#         continue
+#     if i == 8:
+#         break
+#     print(i)
+# print('Цикл завершен')
+#
+# while True:
+#     code = input("Введите 0 для выхода: ")
+#     if code == "0":
+#         break
+i = 0
+while True:
+    i += 1
+
+    print(i)

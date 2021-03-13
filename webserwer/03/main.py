b0ff7869c80c00969655a462abd953d159795d79
Shadow <Shadow-Zy@yandex.ru>
2021-03-14 02:34:31 +0300

    1

--- webserwer/03/main.py
+++ webserwer/03/main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id =db.Column(db.Integer, primary_key=True) #уникальный ключ
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    datd = db.Column(db.DateTime(300), default=datetime.utcnow())
+
+    def __repr__(self):
+        return 'Article %r>' % self.id
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):

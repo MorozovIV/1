04583420a75eff854a1818938768c5a3af6ba4ee
Shadow <Shadow-Zy@yandex.ru>
2023-11-15 15:33:04 +0300

    ui3

--- 0.7/UI.py
+++ 0.7/UI.py
@@ -2,4 +2,4 @@ import eel
 
 eel.init("web")
 #eel.start("main html",size = (400,200), mode="chrome-app", port=9000) #mode = "chrome", edge ,electron, custom
-eel.start('main.html', size=(250,200), position=(500,250)) #size=(400,200)
+eel.start('main.html', size=(250, 200), position=(500, 250)) #size=(400,200)
